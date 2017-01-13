01e4aec499c4864bfa57a26eab2f6bab1fcccc60
Giannis Tholoenos <g.tholoenos@sieben.gr>
2017-01-13 16:49:31 +0200

    added some old extensions

---
+++ Gilegi.Common.Extensions/GuidExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gilegi.Common.Extensions
+{
+    public static class GuidExtensions
+    {
+        public static string StripDashes(this Guid guid)
+        {
+            return guid.ToString("N");
+        }
+    }
+}
---
+++ Gilegi.Common.Extensions/StringExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Gilegi.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string GetLast(this string input, int tailLength)
+        {
+            return tailLength >= input.Length ? input : input.Substring(input.Length - tailLength);
+        }
+
+        public static string Remove(this string input, string value)
+        {
+            return input.Replace(value, string.Empty);
+        }
+
+        public static string RemoveWhitespaces(this string value)
+        {
+            return value.Remove(" ");
+        }
+
+        public static string StripDashes(this string value)
+        {
+            return value.ToLowerInvariant().Replace("-", "");
+        }
+
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        public static Guid ToGuid(this string value)
+        {
+            Guid guid;
+
+            if (Guid.TryParse(value, out guid))
+            {
+                return guid;
+            }
+
+            return Guid.Empty;
+        }
+    }
+}
